364b5eebb2b6116a64dd0a05a9cd7cdd4d70cc7b
Matheus Valbert <matheusvalbert@me.com>
2021-07-08 19:12:59 -0300

    adição Firebase: Cloud Messaging

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'http://10.0.2.2:3333', //Android
-  baseURL: 'http://192.168.50.34:3333' //iOS
+  baseURL: 'http://192.168.50.34:3333'
 });
 
 export default api;
